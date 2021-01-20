d2316d654fe99a8395c0dd9a239233ebcc058f0a
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2021-01-19 21:35:22 -0500

    Update main.cpp
    Using Boyer Moore Algorithm.
    
    Runtime: 8 ms, faster than 99.99% of C++ online submissions for Majority Element.
    Memory Usage: 19.5 MB, less than 97.57% of C++ online submissions for Majority Element.

--- main.cpp
+++ main.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int max = 0;
-        unordered_map<int, int> tempMap;
-        int size = nums.size() / 2;
+        int count = 0;
         
         for (int i = 0; i < nums.size(); ++i) {
-            tempMap[nums[i]]++;
-        }
-        
-        unordered_map<int, int>::iterator iter = tempMap.begin();
-        unordered_map<int, int>::iterator endIter = tempMap.end();
-        
-        for (; iter != endIter; ++iter) {
-            if (iter->second > size) {
-                max = iter->first;
+            if (count == 0) {
+                max = nums[i];
+            }
+            
+            if (nums[i] == max) {
+                count++;
+            }
+            else {
+                count--;
             }
         }
         
